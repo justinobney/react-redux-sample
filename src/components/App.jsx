7fcc44c421b975ad92ad941450bdcec5bbb98506
Justin Obney <justinobney@gmail.com>
2015-09-02 07:32:42 -0500

    feat: move loading bar to own component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,28 +6,53 @@ import TopNav from './TopNav';
 
 class App extends Component {
   displayName = 'App component'
-  _showLoading() {
-    let loading = this.props.uiState.loading;
-    if (loading.pending !== 0) {
-      let percent = (loading.complete / loading.pending) * 100;
-      if (percent < 10) {
-        percent = 5;
-      }
-      return (
-        <ProgressBar active now={percent} />
-      );
-    }
-  }
   render() {
+    let loading = this.props.uiState.loading;
     return (
       <div>
         <TopNav />
         <Grid>
-          {:: this._showLoading()}
+          <LoadingBar pending={loading.pending} complete={loading.complete} />
           {this.props.children}
         </Grid>
       </div>
     );
+  }
+}
+
+class LoadingBar extends Component {
+  displayName = 'LoadingBar component'
+  constructor(props) {
+    super(props);
+    this.state = {pending: props.pending || 0, complete: props.complete || 0};
+  }
+  componentWillReceiveProps(nextProps) {
+    let {pending, complete} = nextProps;
+    let setNextProps = ()=> this.setState({pending, complete});
+    if (this.props.complete > 0 && complete === 0) {
+      this.setState({
+        pending: this.props.pending,
+        complete: this.props.pending
+      });
+
+      setTimeout(setNextProps, 1000);
+    } else {
+      setNextProps();
+    }
+  }
+  render() {
+    let result = null;
+    if (this.state.pending !== 0) {
+      let percent = (this.state.complete / this.state.pending) * 100;
+      if (percent < 10) {
+        percent = 5;
+      }
+      result = (
+        <ProgressBar active now={percent} />
+      );
+    }
+
+    return result;
   }
 }
 
